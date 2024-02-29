168efa83cdae20cf67afb6d3cb3bccfdc48ed2fb
AyanBoii <ayanparashar@gmail.com>
2024-02-29 13:41:23 +0530

    Comitting Primary Files

---
+++ src/main/java/jade/Window.java
@@ -0,0 +1,140 @@
+package jade;
+
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+import util.Time;
+
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+public class Window {
+    private final int width, height;
+    private final String title;
+    private long glfwWindow;
+    private static Window window = null;
+    public float r, g, b, a;
+    private boolean fadeToBlack;
+    private static Scene currentScene;
+
+    private Window () {
+        this.width = 1920;
+        this.height = 1080;
+        this.title = "AyanMasti";
+        r = 1;
+        g = 1;
+        b = 1;
+        a = 1;
+    }
+
+    public static void changeScene(int newScene) {
+        switch (newScene) {
+            case 0:
+                currentScene = new LevelEditorScene();
+                currentScene.init();
+                currentScene.start();
+                break;
+            case 1:
+                currentScene = new LevelScene();
+                currentScene.init();
+                currentScene.start();
+                break;
+            default:
+                assert false: "Unknown Scene" + newScene;
+                break;
+        }
+    }
+
+    public static Window get() {
+        if (Window.window == null) {
+            Window.window = new Window();
+        }
+
+        return Window.window;
+    }
+
+    public static Scene getScene() {
+        return get().currentScene;
+    }
+
+    public void run() {
+        System.out.println("Hello LWJGL "+ Version.getVersion()+" !");
+        init();
+        loop();
+
+        //free the memory
+        glfwFreeCallbacks(glfwWindow);
+        glfwDestroyWindow(glfwWindow);
+
+        // Terminate GLFW and the free the error callback
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+    }
+
+    public void init() {
+        //setup an error callback
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        //initialise glfw
+        if (!glfwInit()) {
+            throw new IllegalStateException("unable to initialise GLFW");
+        }
+
+        //configure GLFW
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+
+        //creating the window
+        glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
+        if (glfwWindow == NULL) {
+            throw new IllegalStateException("Failed to create GLFW window");
+        }
+
+        //setting event listener callbacks
+        glfwSetCursorPosCallback(glfwWindow, MouseListener::mousePosCallback);
+        glfwSetMouseButtonCallback(glfwWindow, MouseListener::mouseButtonCallback);
+        glfwSetScrollCallback(glfwWindow, MouseListener::mouseScrollCallback);
+        glfwSetKeyCallback(glfwWindow, KeyListener::keyCallback);
+
+        //making openGL the current context
+        glfwMakeContextCurrent(glfwWindow);
+        //enabling v-sync
+        glfwSwapInterval(1);
+
+        //making the window visible
+        glfwShowWindow(glfwWindow);
+
+        GL.createCapabilities();
+
+        Window.changeScene(0);
+    }
+
+    public void loop() {
+        float beginTime = Time.getTime();
+        float endTime;
+        float dt = -1.0f;
+
+        while (!glfwWindowShouldClose(glfwWindow)) {
+            //poll events
+            glfwPollEvents();
+
+            //clearing colors and swapping buffers
+            glClearColor(r, g, b, a);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            if (dt >= 0) {
+                currentScene.update(dt);
+            }
+
+            glfwSwapBuffers(glfwWindow);
+
+            endTime = Time.getTime();
+            dt = endTime - beginTime;
+            beginTime = endTime;
+        }
+    }
+}
